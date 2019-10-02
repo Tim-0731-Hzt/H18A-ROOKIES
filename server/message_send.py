dc24cc153664d394509521b4a8aab81ba80938ba
Haodong Lu <lhd32262175@gmail.com>
2019-10-02 15:37:51 +1000

    draft

---
+++ server/message_send.py
@@ -0,0 +1,5 @@
+# Send a message from authorised_user to the channel specified by channel_id
+# ValueError when: Message is more than 1000 characters
+
+def message_send(token, channel_id, message):
+    
---
+++ server/message_send_test.py
@@ -0,0 +1,7 @@
+from message_send import *
+
+# Send a message from authorised_user to the channel specified by channel_id
+# ValueError when: Message is more than 1000 characters
+
+def test_message_send():
+
