de6872a06443547f8f595ea099c36d0559724727
root <root@LAPTOP-TAME7F38.localdomain>
2019-10-02 21:07:43 +1000

    2failed_1passed

---
+++ user_profile_setname_test.py
@@ -0,0 +1,8 @@
+from user_profile_setname import user_profile_setname
+import pytest
+def test1_user_profile_setname():
+    user_profile_setname(123,'Daniel', 'Quin')
+def test2_user_profile_setname():
+    user_profile_setname(123,'Daniel','bgyerfhuqwdcfcfcfcfcfcfcfafffffffffffffffafafaiffffffffffffffffifififififififififififififi')
+def test3_user_profile_setname():
+    user_profile_setname(123,'fyhuiawseoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyoyooooooooooso','Quin')
