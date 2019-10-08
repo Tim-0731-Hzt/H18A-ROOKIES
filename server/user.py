d8a17fff5b7412b1de74d856caeef7bcbd2b2f1d
Haodong Lu <haodong.lu@student.unsw.edu.au>
2019-10-08 20:17:07 +1100

    Delete user.py

--- server/user.py
+++
@@ -1,7 +0,0 @@
-# For a valid user, returns information about their email, first name, last name, and handle
-# ValueError when:
-# User with u_id is not a valid user
-
-def user_profile(token, u_id):
-    pass
-# returned: { email, name_first, name_last, handle_str }
