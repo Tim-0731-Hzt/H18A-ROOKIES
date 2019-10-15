f021535d85da5d15995cc6d5fb54d18183e35b2e
root <root@LAPTOP-TAME7F38.localdomain>
2019-10-16 10:55:36 +1100

    slightly changed

--- server/user.py
+++ server/user.py
@@ -1,9 +1,41 @@
 # For a valid user, returns information about their email, first name, last name, and handle
 # ValueError when:
 # User with u_id is not a valid user
+from Error import AccessError
+userDict = [
+    {
+        'first_name' : 1
+        'last_name' : 1
+        'email' : 1 
+        'u_id' : 1
+        'handle' : 1
+        'password' : 1
+        'token' : 1
+        'online' : True
+        
+    } ,
+    {
+        'first_name' : 2
+        'last_name' : 2
+        'email' : 2
+        'u_id' : 2
+        'handle' : 2
+    },
+    {
+        'first_name' : 3
+        'last_name' : 3
+        'email' : 3
+        'u_id' : 3
+        'handle' : 3
+    }
+]
 
 def user_profile(token, u_id):
-    pass
+    for user in userDict:
+        if user['u_id'] == u_id:
+            return {user['email'], user['firstname'], user['lastname'], user['handle']}
+    raise ValueError('uid was incorrect')
+    return 'incorrect_uid'
 # returned: { email, name_first, name_last, handle_str }
 
 def user_profile_setemail(token, email):
@@ -11,19 +43,50 @@ def user_profile_setemail(token, email):
         raise ValueError('Invalid email')
     if email == 'usedemail' :
         raise ValueError('Used email')
-    pass
+    '''if token == uid'''
+    for user in userDict:
+        if token == user['token']:
+            user['email'] = email
+            return
+    raise ValueError('incorrect token')
+    return 'incorrect_token'
+
+
+
 
 def user_profile_sethandle(token,handle_str):
     if len(handle_str) <= 20 :
         raise ValueError('handle too short')
+    for user in userDict:
+        if token == user['token']:
+            user['handle'] = handle_str
+            return
+    raise ValueError('incorrect token')
+    return 'incorrect_token'
+
+
+
+
 
 def user_profile_setname(token, name_first, name_last):
     if len(name_first) > 50 :
         raise ValueError('First name too long')
     if len(name_last) > 50 :
         raise ValueError('Last name too long')
-    pass
     
+    for user in userDict:
+        if token == user['token']:
+            user['firstname'] = name_first
+            user['lastname'] = name_last
+            return
+    raise ValueError('incorrect token')
+    return 'incorrect_token'
+
+
+
+
+
+
 def user_profiles_uploadphoto(token, img_url, x_start, y_start, x_end, y_end):
     if img_url != 200:
         raise ValueError('url corrupted')
@@ -32,4 +95,4 @@ def user_profiles_uploadphoto(token, img_url, x_start, y_start, x_end, y_end):
         raise ValueError('Out of bound')
     if x_end < 0 or y_end < 0 or x_start < 0 or y_start < 0:
         raise ValueError('Out of bound')
-    pass
+    
