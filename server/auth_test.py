a115e34dfa1db9e02eedb686d52248b690b94735
Chenkai Lyu <1054568553@qq.com>
2019-10-23 23:19:59 +1100

    debugging

--- server/auth_test.py
+++ server/auth_test.py
@@ -6,31 +6,31 @@ from auth import auth_logout
 import auth
 import pytest
 from Error import AccessError
-
+from pickle_unpickle import restart
 
 
 ##
 def test_auth_login_1():
-    registerDict = auth_register('goodemail@gmail.com', '123456','hayden','smith')
+    registerDict = auth_register('goodemail@gmail.com', '123456', 'hayden','smith')
     token1 = registerDict['token']
-   # token1 = token1[2:len(token1)-1]
+    #token1 = token1[2:len(token1)-1]
     auth_logout(token1)
     loginDict = auth_login('goodemail@gmail.com', '123456') 
     u_id1 = loginDict['u_id']
     login_token1 = loginDict['token']
-   # login_token1 = login_token1[2:len(login_token1)-1]
+    #login_token1 = login_token1[2:len(login_token1)-1]
     assert login_token1 == token1
     assert u_id1 == 1
-    assert token1 == 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoxfQ.Bodqy1hnwpsmGtf3MFEhvemrfLLiGQgxuiW4MlbD2WM'
+    assert token1 == "b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoxfQ.Bodqy1hnwpsmGtf3MFEhvemrfLLiGQgxuiW4MlbD2WM'"
 
 def test_auth_login_2():
-    registerDict1 = auth_register('Yuyao@gmail.com', '123456','Yuyao','wang')
-    token1 = registerDict['token']
+    registerDict1 = auth_register('shenjingbing@gmail.com', '123456', 'taobuguo','sb')
+    token1 = registerDict1['token']
     registerDict2 = auth_register('BoA@gmail.com', '123456','BoA','Xv')
-    token2 = registerDict['token']
+    token2 = registerDict2['token']
     auth_logout(token1)
     auth_logout(token2)
-    loginDict1 = auth_login('Yuyao@gmail.com', '123456')
+    loginDict1 = auth_login('shenjingbing@gmail.com', '123456')
     loginDict2 = auth_login('BoA@gmail.com', '123456')
     u_id1 = loginDict1['u_id']
     u_id2 = loginDict2['u_id']
@@ -39,9 +39,11 @@ def test_auth_login_2():
 
     assert login_token1 == token1 
     assert login_token2 == token2
-    assert token1 == 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoxfQ.Bodqy1hnwpsmGtf3MFEhvemrfLLiGQgxuiW4MlbD2WM'
-    assert token2 == 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoyfQ.ZVPCVZoNzgFB9Am_imX_52K6WO_CZf-o8kpsbpdCJl0'
-    
+    assert u_id1 == 2
+    assert u_id2 == 3
+    assert token1 == "b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoxfQ.Bodqy1hnwpsmGtf3MFEhvemrfLLiGQgxuiW4MlbD2WM'"
+    assert token2 == "b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoyfQ.ZVPCVZoNzgFB9Am_imX_52K6WO_CZf-o8kpsbpdCJl0'"
+
 def test_auth_login_invalidEmail():
     with pytest.raises(ValueError, match=r".*"):
         auth_login('soundsbad', '123456')
@@ -49,7 +51,7 @@ def test_auth_login_invalidEmail():
 def test_auth_login_passwordIncorrect(): 
     registerDict = auth_register('good@gmail.com', '123456','hayden','smith')
     with pytest.raises(ValueError, match=r".*"):
-        auth_login('goodmail@gmail.com', '888888')
+        auth_login('good@gmail.com', '888888')
     
 def test_auth_login_notBelongToUser():
     registerDict = auth_register('Jankie@gmail.com', '123456','hayden','smith') 
@@ -62,21 +64,19 @@ def test_auth_logout_1():
     token1 = authRegisterDict['token']
    # token1 = token1[2:len(token1)-1]
     assert auth_logout(token1) == True
-    
+     
 
-""" def test_auth_logout_2():
-    token2 = 'nonvalid'
-    auth_logout(token2)
+
 
 ##
 def test_auth_register_1():
-    authRegisterDict = auth_register('hayden@gmail.com', '123456','hayden','smith')
-    u_id1 = authRegisterDict['u_id']
-    token1 = authRegisterDict['u_id']
-    assert u_id1 == "00001"
-    assert token1 == "1"
+    registerDict = auth_register('xuanhongzhou@gmail.com', '123456','hongzhou','xuan')
+    u_id1 = registerDict['u_id']
+    token1 = registerDict['token']
+    assert u_id1 == 1
+    assert token1 == "b'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1X2lkIjoxfQ.Bodqy1hnwpsmGtf3MFEhvemrfLLiGQgxuiW4MlbD2WM'"
 
-def test_auth_register_2():
+""" def test_auth_register_2():
     authRegisterDict = auth_register('Jankie@gmail.com', '123456','Jankie','Lyu')
     u_id2 = authRegisterDict2['u_id']
     token2 = authRegisterDict2['u_id']
@@ -111,4 +111,4 @@ def test_auth_passwordreset_reset_1():
 def test_auth_passwordreset_reset_2():   
     with pytest.raises(ValueError, match=r".*"):
         auth_passwordreset_reset('asasas', '123456')
- """ 
+  """
