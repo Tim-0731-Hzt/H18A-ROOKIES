1973cbfd5ff7641c5961b6777388f151dbb839a2
Haodong Lu <lhd32262175@gmail.com>
2019-10-25 16:45:52 +1100

    admin bug fixed

--- server/admin_userpermission_change.py
+++ server/admin_userpermission_change.py
@@ -1,17 +1,17 @@
 import pytest
-import error
+import Error
 from auth import *
 import pickle_unpickle
 def admin_userpermission_change(token, u_id, permission_id):
     fl = 1
     data = load()
     userDict = data['userDict']
-    for user in userdict:
+    for user in userDict:
         if u_id == user['u_id']:
             fl = 0
     if fl == 1:
         raise ValueError('Wrong user id')
-    if permission_id != 1 or permission_id != 2 or permission_id != 3:
+    if permission_id not in range(1,3):
         raise ValueError('Unmatch permission id')
     
     opid = getUserFromToken(token)
@@ -23,10 +23,9 @@ def admin_userpermission_change(token, u_id, permission_id):
                 raise AccessError('Permission Denied : member can not do this')
             for sb in userDict:
                 if sb['u_id'] == u_id:
-                    if sb['permissopn_id'] == 1:
-                        raise AccessError('Permission Denied: Trying to change owner  permission')
+                    if sb['permission_id'] == 1:
+                        raise AccessError('Permission Denied: Trying to change owner permission')
                     sb['permission_id'] == permission_id
-                    save(userDict)
-                    return
-
+    save(data)
+    pass
   
