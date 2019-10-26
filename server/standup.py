8b833dd86cf9932dc039c2c1710d9448e8798e74
root <root@LAPTOP-TAME7F38.localdomain>
2019-10-26 13:17:05 +1100

    adding standlist features

--- server/standup.py
+++ server/standup.py
@@ -19,16 +19,22 @@ def standup_start(token, channel_id):
             if ch['standup'] == 1:
                 raise AccessError('this channel is already in standup')
             ch['standup'] == 1
+            save(channelDict)
             t_end = time.time() + 60*15
             while time.time() < t_end:
 
 
             '''time out'''
-            ch['standup'] == 0
-            standlist = load().['standList']
-            message_send(token, channel_id, standlist)
+            channelDt = load().['channelDict']
+            for channel in channelDt:
+                if channel_id == channel['channel_id']:
+                    channel['standup'] == 0
+                    message_send(token, channel_id, channel['standlist'])
+                    channel['standlist'] == ''
+                    save(channelDict)            
             return
     raise ValueError('incorrect channel id')
+    pass
             
 
 def showtime():
@@ -39,7 +45,7 @@ def showtime():
 def standup_send(token, channel_id, message):
     data = load()
     channelDict = data['channelDict']
-    standlist = data['standlist']
+    
     if len(message) > 1000 :
         raise ValueError("Message too long")
     for ch in channelDict:
@@ -49,13 +55,14 @@ def standup_send(token, channel_id, message):
             if ch['standup'] != 1:
                 raise ValueError(
                     'An active standup is not currently running in this channel')
-            if standlist == "":
-                standlist = message
-                save(standlist)
+            if ch['standlist'] == "":
+                ch['standlist'] = message
+                save(channelDict)
                 return
-            else: standlist = standlist + ": " + message
-                save(standlist)
+            else:
+                ch['standlist'] = ch['standlist'] + ": " + message
+                save(channelDict)
                 return
     raise ValueError('Channel ID is not a valid channel')
-
+    pass
     
