68e1b266be625510e6bb7eebba96724ab83c1532
Haodong Lu <lhd32262175@gmail.com>
2019-10-31 14:32:17 +1100

    fixed bugs

--- server/auth_pickle.py
+++ server/auth_pickle.py
@@ -43,7 +43,7 @@ def generateResetCode():
 def generateToken(username):
     global SECRET
     encoded = jwt.encode({'u_id':username},SECRET, algorithm='HS256')
-    encoded = encoded[2:len(token) - 1]
+    encoded = encoded[2:len(encoded) - 1]
     return str(encoded)
 
 def getUserFromToken(token):
